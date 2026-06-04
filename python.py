1c2ee7fb1ecb14394eba03dcc0b73dbd7d9f208b
Salma <2027s.ahadach@burlingtondanes.org>
2026-06-04 09:30:00 +0100

    Add files via upload

---
+++ python.py
@@ -0,0 +1,12 @@
+class Person: #class and object
+  def __init__(self, name, age): #defining properties of object as self name and age
+    self.name = name #defines the name
+    self.age = age #defines the age
+
+p1 = Person("Salma", 17) #creating the class properties
+def greet(self):
+   print("Hello, my name is "Salma" + self.name")
+P1=Person("Salma")
+p1.greet()
+print(p1.name) # prints name
+print(p1.age) # prints age
